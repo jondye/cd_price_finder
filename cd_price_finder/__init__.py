c4ec49ff000d9b5bed19415f1764a30a0a55404c
Jon Dye <jon@pecorous.co.uk>
2020-01-22 16:36:19 +0000

    Initial MusicMagpie implementation

---
+++ cd_price_finder/__init__.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class MusicMagpie:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://www.musicmagpie.co.uk/start-selling/')
+        self.driver.find_element_by_class_name('sellMedia').click()
+
+    def add_barcode(self, barcode):
+        barcode_box = self.driver.find_element_by_class_name('mediaValTextbox')
+        barcode_box.send_keys(barcode)
+        barcode_box.send_keys(Keys.ENTER)
+
+    def results(self):
+        r = []
+        for row in self.driver.find_elements_by_class_name('rowDetails_Media'):
+            barcode = row.find_element_by_class_name('col_Code').text
+            name = row.find_element_by_class_name('col_Title').text
+            price = float(row.find_element_by_class_name('col_Price').text)
+            r.append((barcode, name, price))
+        return r
+
